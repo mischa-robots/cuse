328c7b0ea6d407dc996c6e0836e1f878ed67e101
schobele <leoschoberwalter@gmail.com>
2025-01-14 17:41:32 +0100

    fix: set docker image tag to latest

--- packages/cli/config.ts
+++ packages/cli/config.ts
@@ -4,5 +4,5 @@ export const PROXY_PORT = 4242;
  * A mapping from supported OS to their corresponding Docker image.
  */
 export const imagesByOS: Record<string, string> = {
-  linux: process.env.LINUX_IMAGE || 'ghcr.io/cuse-dev/cuse/linux:main',
+  linux: process.env.LINUX_IMAGE || 'ghcr.io/cuse-dev/cuse/linux:latest',
 };
